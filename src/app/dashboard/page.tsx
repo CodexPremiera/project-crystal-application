b9b20e53ecd73fd4877df4853aa7a4a105225855
CodexPremiera <ashleyken.professional@gmail.com>
2025-08-20 13:28:53 +0800

    fix middleware to properly protect routes requiring auth

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  //Authentication
+  
+  //if account does not exist
+  
+  return <div>DashboardPage</div>
+}
+
+export default DashboardPage
