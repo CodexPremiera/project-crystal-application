419dddc2bc6ef9a1ae0767da64106e9c6b783ca3
CodexPremiera <ashleyken.professional@gmail.com>
2025-08-24 20:48:35 +0800

    make the sidebar responsive

--- src/components/global/sidebar/sidebar.tsx
+++ src/components/global/sidebar/sidebar.tsx
@@ -17,7 +17,7 @@ import {userQueryData} from "@/hooks/useQueryData";
 import {getWorkSpaces} from "@/actions/workspace";
 import {NotificationProps, WorkSpaceProps} from "@/types/index.type";
 import Modal from "@/components/global/modal";
-import {PlusCircle} from "lucide-react";
+import {Menu, PlusCircle} from "lucide-react";
 import Search from "@/components/global/search";
 import {MENU_ITEMS} from "@/constants/constants";
 import SidebarItem from "@/components/global/sidebar/sidebar-item";
@@ -26,6 +26,8 @@ import WorkspacePlaceholder from "@/components/global/sidebar/workspace-placehol
 import GlobalCard from "@/components/global/global-card";
 import {Button} from "@/components/ui/button";
 import Loader from "@/components/global/loader/loader";
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import Infobar from "@/components/global/infobar";
 
 type Props = {
   activeWorkspaceId: string;
@@ -54,7 +56,7 @@ export default function Sidebar({ activeWorkspaceId }: Props) {
   
   const currentWorkspace = workspace?.workspace.find(item => item.id === activeWorkspaceId);
   
-  return (
+  const SidebarSection = (
     <div className="bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden">
       <div className="bg-[#111111] p-4 flex gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0">
         <Image
@@ -214,4 +216,35 @@ export default function Sidebar({ activeWorkspaceId }: Props) {
       )}
     </div>
   );
+  
+  return (
+    <div className="full">
+      <Infobar />
+      
+      <div className="md:hidden fixed my-4">
+        <Sheet>
+          <SheetTrigger
+            asChild
+            className="ml-2"
+          >
+            <Button
+              variant={'ghost'}
+              className="mt-[2px]"
+            >
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side={'left'}
+            className="p-0 w-fit h-full"
+          >
+            {SidebarSection}
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="hidden md:block h-full">
+        {SidebarSection}
+      </div>
+    </div>
+  )
 }
