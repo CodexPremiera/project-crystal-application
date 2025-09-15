68b92b84368768df5dfe22bcd8df2c29bd11cc4e
CodexPremiera <ashleyken.professional@gmail.com>
2025-09-15 14:04:24 +0800

    establish socket connection

--- crystal-express/server.js
+++ crystal-express/server.js
@@ -1,14 +1,28 @@
 const express = require('express')
-const app = express()
-
+const { Server } = require("socket.io");
 const cors = require('cors')
 const http = require('http');
+const app = express()
 
 const server = http.createServer(app);
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 app.use(cors());
 
 
+const io = new Server(server, {
+  cors: {
+    origin: process.env.ELECTRON_HOST || "http://localhost:3000",
+    methods: ['GET', 'POST']
+  }
+});
 
+io.on('connection', (socket) => {
+  console.log('🟢 Socket is Connected: ' + socket.id)
+})
 
-server.listen(5000, () => console.log('listening to port 5000'))
+server.listen(5001, () => {
+  console.log('🟢 Listening to port 5001')
+});
